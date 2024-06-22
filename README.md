77920bb079a15c90c2d94867acd075030ef72074
unknown <robertpierce2828@gmail.com>
2024-06-22 12:46:05 +0800

    24_6_22_tyu

---
+++ README.md
@@ -0,0 +1,13 @@
+# echo "# full-stack-not-show" >> README.md
+
+# git init
+
+# git add README.md
+
+# git commit -m "24_6_22_tyu"
+
+# git checkout -b 24_6_22_tyu
+
+# git remote add origin git@github.com:lushiheng123/full-stack-not-show.git
+
+# git push -u origin 24_6_22_tyu
---
+++ api/app.js
@@ -0,0 +1,42 @@
+import express from "express";
+import cors from "cors";
+
+import cookieParser from "cookie-parser";
+import authRoute from "./routes/auth.route.js";
+import postRoute from "./routes/post.route.js";
+import testRoute from "./routes/test.route.js";
+import userRoute from "./routes/user.route.js";
+import chatRoute from "./routes/chat.route.js";
+import messageRoute from "./routes/message.route.js";
+import "dotenv/config";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/test", testRoute);
+app.use("/api/chats", chatRoute);
+app.use("/api/messages", messageRoute);
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Server is running on port 8081");
+});
